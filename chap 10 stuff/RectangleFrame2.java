8113927ecb07f435fc433e10afa32f2edd9f0e1e
tjhenning <tjhenning@NN123-HA-06.HS.sd203.org>
2016-02-17 15:07:54 -0600

    durr
    durr

--- chap 10 stuff/RectangleFrame2.java
+++ chap 10 stuff/RectangleFrame2.java
@@ -4,6 +4,9 @@ import java.awt.event.MouseListener;
  import javax.swing.JFrame;
   import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
  /**
  This frame contains a moving rectangle.
@@ -12,7 +15,7 @@ public class RectangleFrame2 extends JFrame
     {
      private static final int FRAME_WIDTH = 300;
      private static final int FRAME_HEIGHT = 400;
-    
+     private JButton button=new JButton("Draw");
      private JOptionPane scene=new JOptionPane();
      private CircleComponent2 scene2=new CircleComponent2();
      private int[] xyxy=new int[6];
@@ -35,8 +38,21 @@ public class RectangleFrame2 extends JFrame
     }
      public RectangleFrame2()
      {    
-    
-     add(scene);
+     ClickListener listener = new ClickListener();
+        button.addActionListener(listener);
+         add(button);
+     setSize(100, 100);
+     
+     
+     
+    }
+    public class ClickListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {        
+            remove(button);
+            add(scene);
+            
      String x = JOptionPane.showInputDialog("Input x value: ");
      String y = JOptionPane.showInputDialog("Input y value: ");
      String r = JOptionPane.showInputDialog("Input radius: ");
@@ -45,7 +61,6 @@ public class RectangleFrame2 extends JFrame
      int[] d= {Integer.parseInt(x),Integer.parseInt(y),r2,r2};
      scene2.setCircleTo(d);
      setSize(FRAME_WIDTH, FRAME_HEIGHT);
-     
-     
+        }
     }
 }
--- chap 10 stuff/RectangleViewer2.java
+++ chap 10 stuff/RectangleViewer2.java
@@ -6,9 +6,9 @@
  public class RectangleViewer2
  {
  public static void main(String[] args)
- {
- JFrame frame = new RectangleFrame2();
- frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- frame.setVisible(true);
- }
+    {
+        JFrame frame = new RectangleFrame2();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
  }
